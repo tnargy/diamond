4c484d6088f85a567b95fabaa9c4dc3ba52bff33
Grant <clsscmstng@gmail.com>
2024-01-28 15:06:40 +0000

    using recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
+  Templateurl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
-       recipes = [];
+export class RecipeListComponent implements OnInit {
+       recipes: Recipe[] = [
+       		new Recipe('A Test', 'This is a test.', 'https://www.japanesecooking101.com/wp-content/uploads/2019/04/DSC00452b-1140x640.jpg')
+       ];
 
+       constructor() {}
+
+       ngOnInit() {
+       }
 }
