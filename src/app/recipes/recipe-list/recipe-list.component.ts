f24d8274aa9ead11e075dd50b1d93aca87dbe14d
Grant <clsscmstng@gmail.com>
2024-01-28 15:25:36 +0000

    Fixed case on templateUrl bug

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -3,12 +3,16 @@ import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
-  Templateurl: './recipe-list.component.html',
+  templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit {
        recipes: Recipe[] = [
-       		new Recipe('A Test', 'This is a test.', 'https://www.japanesecooking101.com/wp-content/uploads/2019/04/DSC00452b-1140x640.jpg')
+       		new Recipe(
+            'A Test', 
+            'This is a test.', 
+            'https://www.japanesecooking101.com/wp-content/uploads/2019/04/DSC00452b-1140x640.jpg'
+          )
        ];
 
        constructor() {}
