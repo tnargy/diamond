7c617f4b4864cec99492e0bc081e0671395ab0dd
Grant <clsscmstng@gmail.com>
2024-02-04 16:11:03 +0000

    Made recipes clickable in list and display in the details sections.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,21 +7,26 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit {
-       recipes: Recipe[] = [
-       		new Recipe(
-            'A Test 1', 
-            'This is a test.', 
-            'https://www.japanesecooking101.com/wp-content/uploads/2019/04/DSC00452b-1140x640.jpg'
-          ),
-          new Recipe(
-            'A Test 2', 
-            'This is a test.', 
-            'https://savorthebest.com/wp-content/uploads/2018/07/smoked-salmon-Philadelphia-roll-_2218.jpg'
-          )
-       ];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test 1', 
+      'This is a test.', 
+      'https://www.japanesecooking101.com/wp-content/uploads/2019/04/DSC00452b-1140x640.jpg'
+    ),
+    new Recipe(
+      'A Test 2', 
+      'This is a test.', 
+      'https://savorthebest.com/wp-content/uploads/2018/07/smoked-salmon-Philadelphia-roll-_2218.jpg'
+    )
+  ];
 
-       constructor() {}
+  onRecipeSelected(recipeElement: Recipe) {
+    this.recipeSelected.emit(recipeElement);
+  }
 
-       ngOnInit() {
-       }
+  constructor() {}
+
+  ngOnInit() {
+  }
 }
