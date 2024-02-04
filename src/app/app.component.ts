08e6d40f479a07bbed679fee63d93e71ab3ac8a7
tnargy <clsscmstng@gmail.com>
2024-02-04 04:36:42 +0000

    Header Main Navigation setup and only display one at a time.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
