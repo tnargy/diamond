e869ec6dafdb3f169cfed3cb1ead4fbf06471c11
tnargy <clsscmstng@gmail.com>
2024-02-06 03:46:14 +0000

    Host Listener

--- src/app/better-highlight/better-hightlight.directive.ts
+++ src/app/better-highlight/better-hightlight.directive.ts
@@ -1,13 +1,20 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHightlight]'
 })
 export class BetterHightlightDirective implements OnInit{
-
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+  }
+
+  @HostListener('mouseenter') mouseover(evnetData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+  }
+  
+  @HostListener('mouseleave') mouseleave(evnetData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')
   }
 }
