6a01a4bf927533d6fb806a27bbe3ed9e89eb9ab4
tnargy <clsscmstng@gmail.com>
2024-02-06 04:16:15 +0000

    structure directive and ngSwitch

--- src/app/better-highlight/better-hightlight.directive.ts
+++ src/app/better-highlight/better-hightlight.directive.ts
@@ -1,20 +1,24 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHightlight]'
 })
 export class BetterHightlightDirective implements OnInit{
+  @Input() defaultColor: string  = 'transparent';
+  @Input() highlighColor: string  = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor: string = this.defaultColor;
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseover(evnetData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    this.backgroundColor = this.highlighColor;
   }
   
   @HostListener('mouseleave') mouseleave(evnetData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')
+    this.backgroundColor = this.defaultColor;
   }
 }
