fca6de84a97f8d66d0db6b5115044613eb92c664
tnargy <clsscmstng@gmail.com>
2024-02-06 03:42:53 +0000

    Better Highlight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHightlightDirective } from './better-highlight/better-hightlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
+    BetterHightlightDirective,
   ],
   imports: [
     BrowserModule,
