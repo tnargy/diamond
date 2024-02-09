d47e37865a65f62ad6938383871db3360d4dc0c8
Grant <clsscmstng@gmail.com>
2024-02-09 23:40:23 +0000

    Manage Recipe send to Shopping List

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,7 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
@@ -9,16 +11,32 @@ export class RecipeService {
     new Recipe(
       'A Test 1',
       'This is a test.',
-      'https://www.japanesecooking101.com/wp-content/uploads/2019/04/DSC00452b-1140x640.jpg'
+      'https://www.japanesecooking101.com/wp-content/uploads/2019/04/DSC00452b-1140x640.jpg',
+      [
+        new Ingredient('Bag of Rice', 1),
+        new Ingredient('Egg', 5),
+        new Ingredient('Seaweed', 1)
+      ]
     ),
     new Recipe(
       'A Test 2',
       'This is a test.',
-      'https://savorthebest.com/wp-content/uploads/2018/07/smoked-salmon-Philadelphia-roll-_2218.jpg'
+      'https://savorthebest.com/wp-content/uploads/2018/07/smoked-salmon-Philadelphia-roll-_2218.jpg',
+      [
+        new Ingredient('Bag of Rice', 1),
+        new Ingredient('Fish', 3),
+        new Ingredient('Avocado', 2)
+      ]
     )
   ];
+  
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
