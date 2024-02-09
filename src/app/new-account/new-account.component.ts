f897361f97581e8c5d15df0c0698bd47871bc783
Grant <clsscmstng@gmail.com>
2024-02-09 22:06:30 +0000

    Finished section

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
-import { LoggingService } from '../logging.service';
 import { AccountService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
   constructor(
-    private loggingService: LoggingService,
     private accountServer: AccountService
-  ) {}
+  ) {
+    this.accountServer.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountServer.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
   }
 }
