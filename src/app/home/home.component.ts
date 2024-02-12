50892f425faa9c7456b01e61487909d73befd07d
Grant <clsscmstng@gmail.com>
2024-02-12 21:47:47 +0000

    Working with params

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,8 +10,8 @@ export class HomeComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit() {}
-  
-  onLoadServers() {
-    this.router.navigate(['/servers'])
+
+  onLoadServers(id: number) {
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'})
   }
 }
